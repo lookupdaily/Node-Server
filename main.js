cf1caad8cbb43fc8f21dcf686413d4b8931cd56a
Liz Daly <liz7daly@gmail.com>
2020-06-18 17:47:15 +0100

    Update server to use request url in response

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var date = require('./src/time')
 
 http.createServer((request,response) => {
   response.writeHead(200, {'Content-Type': 'text/html'})
-  response.write('The date and time are currently: ' + date.timeNow())
+  response.write(`<p>${request.url}</p>`)
+  response.write(`The date and time are currently: ${date.timeNow()}`)
   response.end()
 }).listen(8080)
 
