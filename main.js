4a76ab4dfb5865e6da0e3c9b7a8f405c0d8575fc
Liz Daly <liz7daly@gmail.com>
2020-06-18 22:58:35 +0100

    Update server to serve html

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var http = require('http')
 var url = require('url')
+var fs = require('fs')
 
 
 http.createServer((request,response) => {
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  const query = url.parse(request.url, true).query
-  const text = query.year + " " + query.month
-  response.end(text)
+  fs.readFile('index.html', (err, data) => {
+    const query = url.parse(request.url, true).query
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.write(data)
+    response.end()
+  })
+  
+
+ 
 }).listen(8080)
 
